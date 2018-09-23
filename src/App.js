d8423ae4ca3e36e519d9143dc6adf20ec371df34
RyderBlack <ryhad.boughanmi@gmail.com>
2018-09-23 16:29:54 -0400

    add details comp v0.01

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import startups from "./startupopenhouse-mtl2018";
+import Details from './components/Details';
 
 class App extends Component {
 
@@ -12,18 +13,7 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    const config = {
-      headers: {
-        accept: 'application/json',
-      },
-      data: {}
-    };
-    fetch('https://api.startupopenhouse.com/v1/',config)
-    .then(res => {
-      console.log(res.url)
-    })
-  }
+  
   render() {
     return (
       <div className="App">
@@ -31,17 +21,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {this.state.startups.map((startup) => {
-          const linkImg = 'https://images.startupopenhouse.com/thumbnail?colorspace=srgb&width=128&url=https://s3.amazonaws.com/soh-pwa-files-production/';
-          //console.log(startup.props.logo_square.props.uri)
-          return (
-            <div key={startup.props.id}>
-              <img src={linkImg+ startup.props.logo_square.props.uri} alt="logo"/>
-              <p className="App-intro" >{startup.props.name}</p>
-            </div>
-          )
-        })}
-        {/*console.log(this.state.startups)*/}
+        <Details details={this.state.startups} />
         
       </div>
     );
