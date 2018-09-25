5d4f442ad65fda4f1fd6f977d0732fb92184775e
RyderBlack <ryhad.boughanmi@gmail.com>
2018-09-24 20:28:08 -0400

    restructed infos display

--- src/components/Details.js
+++ src/components/Details.js
@@ -11,12 +11,12 @@ export default class Details extends Component {
           const linkImg = 'https://images.startupopenhouse.com/thumbnail?colorspace=srgb&width=128&url=https://s3.amazonaws.com/soh-pwa-files-production/';
          
           return (
-        <div key={coin.props.id} className="col-md-3">
-            <div className={ coin.props.hiring ? "card text-white bg-default mb-3 bg-success" : "card text-white bg-default mb-3 bg-danger"} style={{"maxWidth": "18rem"}}>
-                <div className="card-header">
+        <div key={coin.props.id} className="col-md-10 center">
+            <div className={ coin.props.hiring ? "row text-white bg-success" : "row text-white bg-danger"} >
+                <div className="col-md-2">
                 <img className="card-img" src={linkImg+coin.props.logo_square.props.uri} style={{"maxHeight": "120px"}} alt="logo"/>
                 </div>
-                <div className="card-body" style={{'padding': '15px'}}>
+                <div className="col-md-10" style={{padding: '15px'}}>
                     <h5 className="card-title">{coin.props.name}</h5>
                     <p className="card-text">{coin.props.about}</p>
                 </div>
