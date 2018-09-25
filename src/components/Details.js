b0adf470e392a8dd9a8d3ec72d89386b07d74565
RyderBlack <ryhad.boughanmi@gmail.com>
2018-09-24 20:24:06 -0400

    adding about

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,18 +7,18 @@ export default class Details extends Component {
   render() {
     return (
         <div className="row justify-content-center">
-        {this.props.details.map((startup) => {
+        {this.props.details.map((coin) => {
           const linkImg = 'https://images.startupopenhouse.com/thumbnail?colorspace=srgb&width=128&url=https://s3.amazonaws.com/soh-pwa-files-production/';
-          //console.log(startup.props.logo_square.props.uri)
+         
           return (
-        <div key={startup.props.id} className="col-md-3">
-            <div className={ startup.props.hiring ? "card text-white bg-default mb-3 bg-success" : "card text-white bg-default mb-3 bg-danger"} style={{"maxWidth": "18rem"}}>
+        <div key={coin.props.id} className="col-md-3">
+            <div className={ coin.props.hiring ? "card text-white bg-default mb-3 bg-success" : "card text-white bg-default mb-3 bg-danger"} style={{"maxWidth": "18rem"}}>
                 <div className="card-header">
-                <img className="card-img" src={linkImg+ startup.props.logo_square.props.uri} style={{"maxHeight": "120px"}} alt="logo"/>
+                <img className="card-img" src={linkImg+coin.props.logo_square.props.uri} style={{"maxHeight": "120px"}} alt="logo"/>
                 </div>
                 <div className="card-body" style={{'padding': '15px'}}>
-                    <h5 className="card-title">{startup.props.name}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 className="card-title">{coin.props.name}</h5>
+                    <p className="card-text">{coin.props.about}</p>
                 </div>
             </div>
         </div>
